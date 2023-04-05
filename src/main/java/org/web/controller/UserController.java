e8df89d4d7f6d2016dc087465ff2749cc9f30faa
paketchino <chansgorn@gmail.com>
2023-04-05 23:11:42 +0300

    Fix

--- src/main/java/org/web/controller/UserController.java
+++ src/main/java/org/web/controller/UserController.java
@@ -12,7 +12,7 @@ import org.web.service.UserService;
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
